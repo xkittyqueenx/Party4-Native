75341cf6f2bc41c0ca25cd00d40dcaa8ced0b5d9
xkittyqueenx <hannafischer220@gmail.com>
2026-09-24 15:47:22 +0200

    Commmit

--- Check for Updates.cmd
+++ Check for Updates.cmd
@@ -1,5 +1,5 @@
 @echo off
 cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Update-Game.ps1" -GameFolder "%~dp0"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Update-Game.ps1"
 if errorlevel 1 echo Update failed. Please close the game and check the error above.
 pause
